3c152115cedc931f22cd5a668798296a0ed001f9
Arun Nadar <arunjnadar@gmail.com>
2021-08-02 16:05:28 +0530

    Added .html files from zappycode repo

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class Project(models.Model):
-    """A Portfolio Project"""
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='portfolio/images')
+    image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
 
     def __str__(self):
-        """Return a string representation of the model"""
         return self.title
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-
 from .models import Project
 
-# Create your views here.
 def home(request):
-    """Home page for Portfolio app"""
     projects = Project.objects.all()
-    context = {'projects': projects}
-    return render(request, 'portfolio/home.html', context)
+    return render(request, 'portfolio/home.html', {'projects':projects})
